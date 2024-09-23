0e9db249bfa0441d2f51eb2db1f4081e0ae81853
Milly <liuhongmein@gmail.com>
2024-09-23 12:00:08 -0400

    Image

--- app/_components/Logo.tsx
+++ app/_components/Logo.tsx
@@ -2,12 +2,20 @@
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import logo from "@/public/logo.png";
 
 function Logo() {
   const currentPath = usePathname();
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
-      <Image height="60" width="60" alt="The Wild Oasis logo" src="/logo.png" />
+      {/* <Image height="60" width="60" alt="The Wild Oasis logo" src="/logo.png" /> */}
+      <Image
+        src={logo}
+        alt="The Wild Oasis logo"
+        height="60"
+        width="60"
+        quality={1}
+      />
       <span
         className={` ${
           currentPath === "/" ? "text-amber-300" : ""
