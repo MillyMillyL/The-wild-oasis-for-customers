81070af9ee3e78f5f426bd27fb2157b0f1ef8810
Milly <liuhongmein@gmail.com>
2024-09-24 19:34:42 -0400

    fetch cabins list

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,8 +1,9 @@
 import CabinCard from "@/app/_components/CabinCard";
+import { getCabins } from "../_lib/data-service";
 
-export default function Page() {
+export default async function Page() {
   // CHANGE
-  const cabins = [];
+  const cabins = await getCabins();
 
   return (
     <div>
