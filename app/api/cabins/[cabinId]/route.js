dc3bfc7599e77d9d1ed317f72ece50d9f8cec3fb
Milly <liuhongmein@gmail.com>
2024-09-29 13:29:04 -0400

    route handlers

---
+++ app/api/cabins/[cabinId]/route.js
@@ -0,0 +1,15 @@
+import { getCabin, getBookedDatesByCabinId } from "@/app/_lib/data-service";
+
+export async function GET(req, { params }) {
+  const { cabinId } = params;
+
+  try {
+    const [cabin, bookedDates] = await Promise.all([
+      getCabin(cabinId),
+      getBookedDatesByCabinId(cabinId),
+    ]);
+    return Response.json({ cabin, bookedDates });
+  } catch (error) {
+    return Response.json({ message: "Cabin not found" });
+  }
+}
